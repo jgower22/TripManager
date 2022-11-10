e27939e479a4952c73d0329856ed1bf8d3951585
jackson.gower@gmail.com <jackson.gower@gmail.com>
2022-11-10 17:12:05 -0500

    connect to render

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const app = express();
 
 //configure app
 let port = process.env.PORT || 8084;
-let host = 'tripsmanager.herokuapp.com';
+let host = 'tripmanager177.onrender.com';
 let url = 'mongodb+srv://ezYCoVwNqUYpADr3eOqgx:awD9i0uSHIIcbsJwtKd79RRpjNTBMqlGMCIJdNFL9ZnxL@trips.jiospgv.mongodb.net/trips';
 //let url = 'mongodb://localhost:27017/trips';
 app.set('view engine', 'ejs');
