3f1aba4014f9a6a16e7c99e19045afa3505628af
jackson.gower@gmail.com <jackson.gower@gmail.com>
2022-12-12 16:50:27 -0500

    location change for all days option

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,28 +1,14 @@
 const User = require('../models/user');
 
 exports.index = (req, res) => {
-    let id = req.session.user;
-    User.findById(id)
-    .then(user => {
-        res.render('./index', {user});
-    })
-    .catch(err=>next(err));
+    res.render('./index');
 };
 
 exports.about = (req, res) => {
-    let id = req.session.user;
-    User.findById(id)
-    .then(user => {
-        res.render('./about', {user});
-    })
-    .catch(err=>next(err));
+    res.render('./about');
 };
 
 exports.contact = (req, res) => {
-    let id = req.session.user;
-    User.findById(id)
-    .then(user => {
-        res.render('./contact', {user});
-    })
-    .catch(err=>next(err));
+    res.render('./contact');
+
 };
--- public/javascript/leaflet.js
+++ public/javascript/leaflet.js
@@ -18,7 +18,7 @@ try {
     console.log('ERROR');
 }
 locationToSet = locationToSet.substring(locationToSet.indexOf(':') + 1, locationToSet.length).trim();
-console.log('LOCATION: ' + locationToSet);
+//console.log('LOCATION: ' + locationToSet);
 
 // build the URL to the geonames API including the name that was entered
 var geonamesURL = 'http://secure.geonames.org/searchJSON?q=' +
@@ -34,7 +34,7 @@ fetch(geonamesURL)
 
     .then((data) => {
 
-        console.log('DATA: ' + JSON.stringify(data));
+        //console.log('DATA: ' + JSON.stringify(data));
 
         if (data.totalResultsCount === 0) {
             map.setView([0, 0], 12);
@@ -45,7 +45,7 @@ fetch(geonamesURL)
         } else {
             // loop through the "N" items returned in the data array
             for (var i = 0; i < data.geonames.length; i++) {
-                console.log(data.geonames[i].name);
+                //console.log(data.geonames[i].name);
 
                 if (i == 0)  {
                     // Centers the view on the first location
