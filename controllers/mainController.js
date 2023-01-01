23c6d0d80ab4691b0ac4261c70461941074dd930
jackson.gower@gmail.com <jackson.gower@gmail.com>
2023-01-01 13:58:54 -0500

    title and favicon

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,14 +1,16 @@
 const User = require('../models/user');
 
 exports.index = (req, res) => {
+    res.locals.title = 'Trip Manager - Home';
     res.render('./index');
 };
 
 exports.about = (req, res) => {
+    res.locals.title = 'Trip Manager - About';
     res.render('./about');
 };
 
 exports.contact = (req, res) => {
+    res.locals.title = 'Trip Manager - Contact';
     res.render('./contact');
-
 };
